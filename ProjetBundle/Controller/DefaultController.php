b578d9ed9b83e6de5b30e32af83ab94058396643
Ryomashiba <clement.gibert@ynov.com>
2016-11-02 10:23:25 +0100

    ajoute de la connexion a la base de donnee

--- ProjetBundle/Controller/DefaultController.php
+++ ProjetBundle/Controller/DefaultController.php
@@ -3,11 +3,26 @@
 namespace ProjetSymfony\ProjetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Bach\BackofficeBundle\Entity\Utilisateur;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        return $this->render('ProjetBundle:Default:index.html.twig');
+    }
+    public function BaseDeDonneAction()
+    {
+        $mail = "";
+        $password = "";
+        $name = "";
+
+        $repo = $this->getDoctrine()->getRepository("ProjetBundle:Admin");
+        $utilisateur = $repo->findOneBy(array('mail'=> $mail, 'password'=> $password, 'name' =>$name));
+
         return $this->render('ProjetBundle:Default:index.html.twig');
     }
 }
