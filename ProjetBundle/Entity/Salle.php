a34a87d2da4ba0299ddef95dd738733f0010a10e
poimulaz <pierre.bezancon@ynov.com>
2016-11-03 09:32:08 +0100

    link database(directory entity and repository)

---
+++ ProjetBundle/Entity/Salle.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace ProjetSymfony\ProjetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Salle
+ *
+ * @ORM\Table(name="salle")
+ * @ORM\Entity(repositoryClass="ProjetSymfony\ProjetBundle\Repository\SalleRepository")
+ */
+class Salle
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="machines", type="int", length=100)
+     */
+    private $machines;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_creation", type="datetime")
+     */
+    private $dateCreation;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Salle
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set machines
+     *
+     * @param \int $machines
+     *
+     * @return Salle
+     */
+    public function setMachines(\int $machines)
+    {
+        $this->machines = $machines;
+
+        return $this;
+    }
+
+    /**
+     * Get machines
+     *
+     * @return \int
+     */
+    public function getMachines()
+    {
+        return $this->machines;
+    }
+
+    /**
+     * Set dateCreation
+     *
+     * @param \DateTime $dateCreation
+     *
+     * @return Salle
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation
+     *
+     * @return \DateTime
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+}
+
