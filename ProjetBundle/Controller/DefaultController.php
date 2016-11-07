fe584da1c704ef5f4bf0cada8b534d6c217ce6ca
axel <sliva.axel78@gmail.com>
2016-11-07 15:04:27 +0100

    connexion

--- ProjetBundle/Controller/DefaultController.php
+++ ProjetBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace ProjetSymfony\ProjetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+
+    public function indexAction(Request $request)
     {
-        return $this->render('ProjetBundle:Default:index.html.twig');
+    	if($request->isMethod("POST")) {
+    		$email = $request->request->get('mail');
+    		$password = $request->request->get('password');
+    		$repo= $this->getDoctrine()->getRepository("ProjetBundle:Utilisateur");
+    		$admin = $repo->findOneBy(array('mail' => $mail, 'password' => $password));
+
+    		if($admin == null)
+    			return $this->render('ProjetBundle_default:index.html.twig');
+    		else
+    			return $this->redirectToRoute('ProjetBundle_default:index.html.twig');
+    	}
+    	else
+    		 return $this->render('ProjetBundle:Default:index.html.twig');
     }
 }
