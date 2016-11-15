d6e64c6ecd180aee12ac5ea0dc6d80fc02fcdf44
Ryomashiba <clement.gibert@ynov.com>
2016-11-15 16:45:28 +0100

    site operationnel sans session

--- ProjetBundle/Controller/DefaultController.php
+++ ProjetBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace ProjetSymfony\ProjetBundle\Controller;
 
 use ProjetSymfony\ProjetBundle\Entity\Admin;
+use ProjetSymfony\ProjetBundle\Entity\Salle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Bach\BackofficeBundle\Entity\Utilisateur;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,34 +12,29 @@ use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
+    public static $local;
+
     public function indexAction()
     {
+        $repo = $this->getDoctrine()->getRepository("ProjetBundle:Salle");
+        $utilisateurs = $repo->findAll();
+        $response = $this->get('templating')
+            ->render('ProjetBundle:Default:index.html.twig', array('utilisateurs'=>$utilisateurs));
+        return new Response($response);
+    }
+
+    public function ChoixSalleAction(Request $request)
+    {
+        if ($request->isMethod('POST'))
+        {
+            DefaultController::$local= $request->get('yolo');
+            $repo = $this->getDoctrine()->getRepository("ProjetBundle:Machine");
+            $utilisateurs = $repo->findBy(array('salle'=> DefaultController::$local ));
+            $response = $this->get('templating')
+                ->render('ProjetBundle:Default:salle.html.twig', array('utilisateurs'=>$utilisateurs));
+            return new Response($response);
+        }
         return $this->render('ProjetBundle:Default:index.html.twig');
     }
-    public function BaseDeDonneAction(Request $request)
-    {
-        $repository = $this->getDoctrine()->getRepository('AppBundle:Admin');
-        $admin = $repository->find(1);
 
-
-
-        return $this->render('ProjetBundle:Default:test.html.twig');
-    }
-    
-
-
-
-    public function createAction()
-    {
-        $u = new Admin();
-        $u->setMail("bob@mail.com");
-        $u->setPassword("qwerty");
-        $u->setName("Bob");
-
-        $datacontext = $this->getDoctrine()->getEntityManager();
-        $datacontext->persist($u);
-        $datacontext->flush();
-
-        return $this->render('ProjetBundle:Default:index.html.twig');
-    }
 }
