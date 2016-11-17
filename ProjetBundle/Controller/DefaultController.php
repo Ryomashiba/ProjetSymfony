2d51405afd196a6c41a564a9ebc36b20adc1cb64
GRANDEMANGE Karim <karim.grandemange@ynov.com>
2016-11-17 14:18:18 +0100

    toto

--- ProjetBundle/Controller/DefaultController.php
+++ ProjetBundle/Controller/DefaultController.php
@@ -2,12 +2,34 @@
 
 namespace ProjetSymfony\ProjetBundle\Controller;
 
+use ProjetSymfony\ProjetBundle\Entity\Admin;
+use ProjetSymfony\ProjetBundle\Entity\Salle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Bach\BackofiiceBundle\Entity\Utilisateur;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Contraints\Datetime;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ProjetBundle:Default:index.html.twig');
+        $repo = $this->getDoctrine()->getRepository("ProjetBundle:Machine");
+        $utilisateurs = $repo->findAll();
+        $response = $this->get('templating')->render('PrjetBundle:Default:index.html.twig', array('utilisateurs'=>$utilisateurs));
+        return new Response($response);
     }
+
+    public function  Machine(Request $request){
+        if ($request->isMethod('POST')){
+            DefaultController::$local = $request->get('yolo');
+            $repo = $this->getDoctrine()->getRepository("ProjetBundle:Machine");
+            $utilisateurs = $repo->findBy(array('machine> DefaultController::$local));
+            $reponse = $this->get('templating')->render('ProjetBundle:Deafault:machine.html.twig', array('utilisateurs'=>$utilisateurs));
+            return new Response($response);
+        }
+
+        return $this->render('BachBundle:Default:index.html.twig');
+    }
+
 }
